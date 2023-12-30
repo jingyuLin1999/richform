3423f94ef2841866f0bd48f856f7127101a74698
jingyuLin1999 <1071711404@qq.com>
2023-12-30 11:30:12 +0800

    1、add teleport and fix time

--- src/components/Richform/widgets/timeselect.vue
+++ src/components/Richform/widgets/timeselect.vue
@@ -3,10 +3,10 @@
   <div class="time-select-widget" ref="timeSelect" :id="widgetId">
     <div class="time-input">
       <div><i class="el-icon-time"></i> </div>
-      <input class="input" v-model="value" type="text" autocomplete="off" name=""
-        :readonly="field.valueFormat == 'timestamp'" placeholder="任意时间点" @focus="() => onFocus(value)">
+      <input class="input" v-model="value" type="text" autocomplete="off" :readonly="field.valueFormat == 'timestamp'"
+        placeholder="任意时间点" @focus="onFocus">
     </div>
-    <div class="time-select-panel" v-show="showTimeSelect">
+    <Teleport v-if="showTimeSelect" ref="teleport" to="body" class="time-select-panel">
       <div class="options">
         <el-scrollbar style="height: 100%" wrap-style="overflow-x:hidden;" ref="hoursScrollContainer"
           @wheel.native.prevent="(e) => { handleScroll(e, 'hours') }">
@@ -37,18 +37,19 @@
         <button type="button" class="el-time-panel__btn cancel" @click="onCancel">取消</button>
         <button type="button" class="el-time-panel__btn confirm" @click="onConfirm">确定</button>
       </div>
-    </div>
+    </Teleport>
   </div>
 </template>
 
 <script>
+import { type, isNil } from "ramda"
 import baseMixin from "./baseMixin";
 import ElScrollbar from 'element-ui/packages/scrollbar';
-import { type } from "ramda"
+import Teleport from "@/components/Teleport"
 
 export default {
   mixins: [baseMixin],
-  components: { ElScrollbar },
+  components: { ElScrollbar, Teleport },
   data() {
     return {
       showTimeSelect: false, // 
@@ -75,7 +76,10 @@ export default {
       let value = this.values[this.field.name];
       if (type(value) == "Number") {
         return this.secondTohms(value)
+      } else if (isNil(value)) {
+        value = "00:00:00"
       }
+
       return value;
     },
     beforeChange(val) {
@@ -89,7 +93,7 @@ export default {
         m = parseInt(m) < 0 ? 0 : parseInt(m) > 59 ? 59 : m
         s = parseInt(s) < 0 ? 0 : parseInt(s) > 59 ? 59 : s
         val = [h, m, s].join(":");
-        this.onFocus(val)
+        this.hmsScrollToNum(val)
       }
       return val;
     },
@@ -137,7 +141,8 @@ export default {
     registerAddEventListener() {
       this.$nextTick(() => {
         window.addEventListener("click", (event) => {
-          if (this.$refs.timeSelect && !this.$refs.timeSelect.contains(event.target)) {
+          if (!this.$refs.teleport || !this.$refs.timeSelect) return;
+          if (!this.$refs.teleport.$el.contains(event.target) && !this.$refs.timeSelect.contains(event.target)) {
             this.showTimeSelect = false
           }
         })
@@ -154,7 +159,7 @@ export default {
       this.value = `${this.formate(this.hours)}:${this.formate(this.minute)}:${this.formate(this.second)}`
       this.onCancel()
     },
-    onFocus(val) {
+    hmsScrollToNum(val) {
       let updateDataKey = ["hours", "minute", "second"]
       let toValue = val ? val : this.value
       let valueArr = toValue.split(":");
@@ -164,6 +169,26 @@ export default {
         this.scrollToNum(this.formate(this[key]), key);
       })
     },
+    onFocus(evt) {
+      this.showTimeSelect = true
+      setTimeout(() => {
+        const { left, top } = this.getPosOffBody(evt.target);
+        const el = this.$refs.teleport.$el;
+        el.style.left = `${left - 15}px`;
+        el.style.top = `${top + evt.target.offsetHeight + 5}px`;
+        this.hmsScrollToNum()
+      }, 100);
+    },
+    getPosOffBody(obj) { //获取某元素以浏览器左上角为原点的坐标
+      var t = obj.offsetTop; //获取该元素对应父容器的上边距
+      var l = obj.offsetLeft; //对应父容器的上边距
+      //判断是否有父容器，如果存在则累加其边距
+      while (obj = obj.offsetParent) {//等效 obj = obj.offsetParent;while (obj != undefined)
+        t += obj.offsetTop; //叠加父容器的上边距
+        l += obj.offsetLeft; //叠加父容器的左边距
+      }
+      return { left: l, top: t }
+    }
   },
 };
 </script>
@@ -172,10 +197,6 @@ export default {
 .time-select-widget {
   width: 100%;
   position: relative;
-
-  .el-scrollbar__bar.is-horizontal {
-    height: 0px !important;
-  }
 
   >.time-input {
     border: 1px solid #dcdfe6;
@@ -193,89 +214,92 @@ export default {
       margin-left: 3px;
     }
   }
+}
 
-  >.time-select-panel {
-    position: absolute;
-    bottom: -228px;
-    left: 0;
-    z-index: 999;
-    width: 185px;
-    height: 225px;
-    background-color: #fff;
-    box-shadow: 0 0 5px #ccc;
+.time-select-panel {
+  position: absolute;
+  bottom: 0;
+  left: -2000px;
+  z-index: 3000;
+  width: 185px;
+  height: 225px;
+  background-color: #fff;
+  box-shadow: 0 0 5px #ccc;
 
-    >.options {
-      height: 195px;
-      width: 100%;
-      display: flex;
-      justify-content: space-between;
-      box-sizing: border-box;
-      padding: 0 5px;
-      position: relative;
+  .el-scrollbar__bar.is-horizontal {
+    height: 0px !important;
+  }
 
-      ul,
-      li {
-        margin: 0;
-        padding: 0;
-      }
+  >.options {
+    height: 195px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    box-sizing: border-box;
+    padding: 0 5px;
+    position: relative;
 
-      .panel-ul {
+    ul,
+    li {
+      margin: 0;
+      padding: 0;
+    }
+
+    .panel-ul {
+      list-style: none;
+      width: 60px;
+
+      .panel-li {
         list-style: none;
-        width: 60px;
+        height: 32px;
+        width: 55px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        color: #444;
+        font-size: 13px;
 
-        .panel-li {
-          list-style: none;
-          height: 32px;
-          width: 55px;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          cursor: pointer;
-          color: #444;
-
-          &:hover {
-            background-color: #F5F7FA;
-          }
-        }
-
-        .active {
-          color: #000;
+        &:hover {
+          background-color: #F5F7FA;
         }
       }
+
+      .active {
+        color: #000;
+      }
     }
+  }
 
-    >.options::after {
-      content: "";
-      position: absolute;
-      top: calc(50% - 3px);
-      left: 0;
-      width: 100%;
-      height: 1px;
-      background-color: #dcdfe6;
-      padding: 0 5px;
-      box-sizing: border-box;
-    }
+  >.options::after {
+    content: "";
+    position: absolute;
+    top: calc(50% - 3px);
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: #dcdfe6;
+    padding: 0 5px;
+    box-sizing: border-box;
+  }
 
-    >.options::before {
-      content: "";
-      position: absolute;
-      top: calc(50% + 30px);
-      left: 0;
-      width: 100%;
-      height: 1px;
-      background-color: #dcdfe6;
-      padding: 0 5px;
-      box-sizing: border-box;
-    }
+  >.options::before {
+    content: "";
+    position: absolute;
+    top: calc(50% + 30px);
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: #dcdfe6;
+    padding: 0 5px;
+    box-sizing: border-box;
+  }
 
-    >.buttons {
-      border-top: 1px solid #dcdfe6;
-      height: 30px;
-      display: flex;
-      justify-content: flex-end;
-    }
-
-
+  >.buttons {
+    border-top: 1px solid #dcdfe6;
+    height: 30px;
+    display: flex;
+    justify-content: flex-end;
   }
 }
 </style>
---
+++ src/components/Teleport/index.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    props: {
+        to: {
+            type: String,
+            required: true
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    render() {
+        // $scopedSlots用来访问作用域插槽。对于包括 默认 slot 在内的每一个插槽，该对象都包含一个返回相应 VNode 的函数。
+        return <div class="Teleport">{this.$scopedSlots.default()}</div>
+    },
+    watch: {
+        disabled() {
+            if (!this.disabled) {
+                // this指向组件的实例。$el指向当前组件的DOM元素。
+                document.querySelector(this.to).appendChild(this.$el);
+            }
+        }
+    },
+    mounted() {
+        if (!this.disabled) document.querySelector(this.to).appendChild(this.$el)
+    },
+};
+</script>
+<style lang="scss" scoped></style>
